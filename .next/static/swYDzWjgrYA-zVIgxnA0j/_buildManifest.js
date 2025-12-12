4c115084ea4a9598e582aa180f009bf3bd4850a4
Milton-Ventura <165446124+Milton-Ventura@users.noreply.github.com>
2025-12-12 16:20:15 +0000

    build package

--- .next/static/swYDzWjgrYA-zVIgxnA0j/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-bead0500ae96e4ed.js"],"/_error":["static/chunks/pages/_error-dfcfa5bb62767c20.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
